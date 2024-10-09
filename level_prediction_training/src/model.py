52e10b325f45bb9dc6dd50c2d59c1e980570e450
Edward Jex <edwardjex@live.co.uk>
2024-10-09 14:53:20 +0100

    move buffers to device

--- level_prediction_training/src/model.py
+++ level_prediction_training/src/model.py
@@ -87,11 +87,13 @@ class BaseTimeSeriesModel(LightningModule):
         self.n_context_features = len(input_column_names)
         self.x_column_names = input_column_names
 
-        self.quantiles = torch.tensor(config.quantiles, dtype=torch.float32)
+        self.quantiles = torch.tensor(
+            config.quantiles, dtype=torch.float32, device=self.device
+        )
 
         self.register_buffer(
             "thresholds",
-            torch.tensor(config.thresholds, dtype=torch.float32),
+            torch.tensor(config.thresholds, dtype=torch.float32, device=self.device),
         )
 
     def forward(
